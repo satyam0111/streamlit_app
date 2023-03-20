164310d1abfb8737b8f0bd0692157bc3781c8784
satyam0111 <92117949+satyam0111@users.noreply.github.com>
2023-03-21 02:52:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ city = st.text_input('Where to ')
 st.write('You want to go', city)
 condition = df['City'] == city
 new_data = df[condition]
-place=new_data['Place']
-col_values = new_data['Place'].tolist()
-for values in col_values:
-  st.write(values)
+
+col1_values = new_data['Place'].tolist()
+col2_values = new_data['City'].tolist()
+for i in range(len(col1_values)):
+  st.write(col1_values[i],"\t\t\t",col2_values[i])
