2e2987f21a9b32d70ac31dec0ac229127630da69
satyam0111 <92117949+satyam0111@users.noreply.github.com>
2023-03-21 02:35:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,7 @@ city = st.text_input('Where to ')
 st.write('You want to go', city)
 condition = df['City'] == city
 new_data = df[condition]
-st.write(new_data['Place'])
-st.dataframe(new_data)
+place=new_data['Place']
+new_list=place.rows.tolist()
+for values in new_list:
+  st.write(values)
