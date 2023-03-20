e45e1c29aeaaf809e08d6bd4cff95b2beb651b84
satyam0111 <92117949+satyam0111@users.noreply.github.com>
2023-03-21 02:24:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ st.write(df.head())
 city = st.text_input('Where to ')
 st.write('You want to go', city)
 condition = df['City'] == city
-new_data= df[condition]
+new_data = df[condition]
 
 column_names = df.columns.tolist()
 selected_column = st.selectbox("Select column to display", column_names)
-st.write(df[selected_column])
+st.write(new_data[selected_column])
