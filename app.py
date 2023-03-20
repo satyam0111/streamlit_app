da7831254bb14834a4fd9f63825fef3c33010a8e
satyam0111 <92117949+satyam0111@users.noreply.github.com>
2023-03-21 02:12:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 st.write("""First App""")
-df = pd.read_csv("data_places.csv") 
-st.write(df.head())
+data = pd.read_csv("data_places.csv") 
+st.write(data.head())
 city = st.text_input('Where to ')
 st.write('You want to go', city)
 condition = data['City'] == city
 new_data = data[condition]
-t.write(new_data['Place'])
+st.write(new_data['Place'])
