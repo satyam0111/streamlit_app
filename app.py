a4cbd45b8e67d83daa72eb355b154920ef1aecb5
satyam0111 <92117949+satyam0111@users.noreply.github.com>
2023-03-21 02:22:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
 st.write("""First App""")
-data = pd.read_csv("data_places.csv") 
-st.write(data.head())
+df = pd.read_csv("data_places.csv") 
+st.write(df.head())
 city = st.text_input('Where to ')
 st.write('You want to go', city)
-condition = data['City'] == city
-new_data = data[condition]
+condition = df['City'] == city
+new_data= df[condition]
 
 column_names = df.columns.tolist()
 selected_column = st.selectbox("Select column to display", columns_names)
