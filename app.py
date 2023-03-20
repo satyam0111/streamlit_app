a4f69144699dd5db077136878269fcb79abad771
satyam0111 <92117949+satyam0111@users.noreply.github.com>
2023-03-21 01:58:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 st.write("""First App""")
 df = pd.read_csv("data_places.csv") 
-st.write(df)
-city = st.text_input('Where to ', 'City')
+st.write(df.head())
+city = st.text_input('Where to ')
 st.write('You want to go', city)
