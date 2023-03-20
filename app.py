c2941bf9907dea0103aef2c5e5e3f322d151a702
satyam0111 <92117949+satyam0111@users.noreply.github.com>
2023-03-21 02:54:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 st.write("""First App""")
 df = pd.read_csv("data_places.csv") 
-st.write(df.head())
 city = st.text_input('Where to ')
 st.write('You want to go', city)
 condition = df['City'] == city
@@ -12,3 +11,15 @@ col1_values = new_data['Place'].tolist()
 col2_values = new_data['City'].tolist()
 for i in range(len(col1_values)):
   st.write(col1_values[i],"\t\t\t",col2_values[i])
+
+ 
+from googlesearch import search
+
+query = st.text_input('Enter your query:')
+num_results = st.slider('Number of results to display:', value=10, min_value=1, max_value=50)
+
+if st.button('Search'):
+    st.write(f'Searching for "{query}"...')
+    results = search(query, num_results=num_results)
+    for result in results:
+        st.write(result)
