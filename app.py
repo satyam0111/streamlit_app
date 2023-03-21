0c73873d299af3bbc5a96c75946ed1937bf4ed36
Satyam Atreja <92117949+satyam0111@users.noreply.github.com>
2023-03-21 13:37:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,18 +11,3 @@ col1_values = new_data['Place'].tolist()
 col2_values = new_data['City'].tolist()
 for i in range(len(col1_values)):
   st.write(col1_values[i],"\t\t\t",col2_values[i])
-
-
-import requests
-
-search_item = st.text_input("Enter search query")
-
-# Fetch search results page
-url = f"https://www.google.com/search?q={search_item}"
-page = requests.get(url)
-
-
-# Find all search result links and print them
-results = soup.find_all("div", class_="BNeawe vvjwJb AP7Wnd")
-for result in results:
-    st.write(result.get_text())
