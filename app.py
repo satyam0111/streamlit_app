780563ce73e1fc3074f4a91dbab7e027acf5bdb0
Satyam Atreja <92117949+satyam0111@users.noreply.github.com>
2023-03-21 13:51:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-st.write("""First App""")
+st.write("""Travify - Place Recommendation Sytem""")
+st.write("""\tExplore the beauty with us """)
 df = pd.read_csv("data_places.csv") 
 city = st.text_input('Where to ')
 # st.write('You want to go', city)
@@ -10,5 +11,6 @@ new_data = df[condition]
 # reading the data and displaying the corresponding
 col1_values = new_data['Place'].tolist()
 col2_values = new_data['City'].tolist()
+st.write("""Places you can visit in """,city)
 for i in range(len(col1_values)):
   st.write(col1_values[i])
