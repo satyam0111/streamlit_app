cd8e741c5283e95d999170e4c4f9f706014d677a
Satyam Atreja <92117949+satyam0111@users.noreply.github.com>
2023-03-21 13:39:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import pandas as pd
 st.write("""First App""")
 df = pd.read_csv("data_places.csv") 
 city = st.text_input('Where to ')
-st.write('You want to go', city)
+# st.write('You want to go', city)
 condition = df['City'] == city
 new_data = df[condition]
 
+# reading the data and displaying the corresponding
 col1_values = new_data['Place'].tolist()
 col2_values = new_data['City'].tolist()
 for i in range(len(col1_values)):
-  st.write(col1_values[i],"\t\t\t",col2_values[i])
+  st.write(col1_values[i])
